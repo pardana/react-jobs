ef53773f388890f8f34f8a6f93424d37def58acb
Aditria Pardana <aditria.pardana@yahoo.co.id>
2024-03-27 16:23:57 +0700

    FEAT: Update router jobs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             {/* <!-- Logo --> */}
             <a
               className="flex flex-shrink-0 items-center mr-4"
-              href="/index.html"
+              href="/"
             >
               <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
@@ -19,19 +19,19 @@ const Navbar = () => {
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <a
-                  href="/index.html"
+                  href="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Home
                 </a>
                 <a
-                  href="/jobs.html"
+                  href="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Jobs
                 </a>
                 <a
-                  href="/add-job.html"
+                  href="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Add Job
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JobsPage = () => {
+  return (
+    <div>
+      <h1>Jobs page</h1>
+    </div>
+  )
+}
+
+export default JobsPage
