6513704bd99e39571d6d582bce7bd7f0e6396e99
Aditria Pardana <aditria.pardana20@gmail.com>
2024-04-22 10:30:05 +0700

    FEAT: update JobsPage

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,11 +1,11 @@
-// import React from 'react'
+import JobListings from "../components/JobListings";
 
 const JobsPage = () => {
   return (
-    <div>
-      <h1>Jobs page</h1>
-    </div>
-  )
-}
+    <section className="bg-blue-50 px-4 py-6">
+      <JobListings />
+    </section>
+  );
+};
 
-export default JobsPage
+export default JobsPage;
